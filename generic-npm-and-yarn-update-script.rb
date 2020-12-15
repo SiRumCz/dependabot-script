ad07eb17a334149cf14e8fd5affdff8ba0588171
SiRumCZ <sirumcz@gmail.com>
2020-12-14 21:46:31 -0800

    added support for issue_rem creator

--- generic-npm-and-yarn-update-script.rb
+++ generic-npm-and-yarn-update-script.rb
@@ -6,8 +6,8 @@ require "dependabot/file_parsers"
 require "dependabot/update_checkers"
 require "dependabot/file_updaters"
 require "dependabot/pull_request_creator"
+require "dependabot/rem_issue_creator"
 require "dependabot/omnibus"
-require "gitlab"
 require_relative "vulnerability_fetcher"
 
 credentials = [
@@ -42,68 +42,75 @@ directory = ENV["DIRECTORY_PATH"] || "/"
 # - submodules
 # - docker
 # - terraform
-package_manager = ENV["PACKAGE_MANAGER"] || "bundler"
+package_manager = ENV["PACKAGE_MANAGER"] || "npm_and_yarn"
 
-if ENV["GITHUB_ENTERPRISE_ACCESS_TOKEN"]
-  credentials << {
-    "type" => "git_source",
-    "host" => ENV["GITHUB_ENTERPRISE_HOSTNAME"], # E.g., "ghe.mydomain.com",
-    "username" => "x-access-token",
-    "password" => ENV["GITHUB_ENTERPRISE_ACCESS_TOKEN"] # A GHE access token with API permission
-  }
+# if ENV["GITHUB_ENTERPRISE_ACCESS_TOKEN"]
+#   credentials << {
+#     "type" => "git_source",
+#     "host" => ENV["GITHUB_ENTERPRISE_HOSTNAME"], # E.g., "ghe.mydomain.com",
+#     "username" => "x-access-token",
+#     "password" => ENV["GITHUB_ENTERPRISE_ACCESS_TOKEN"] # A GHE access token with API permission
+#   }
 
-  source = Dependabot::Source.new(
-    provider: "github",
-    hostname: ENV["GITHUB_ENTERPRISE_HOSTNAME"],
-    api_endpoint: "https://#{ENV['GITHUB_ENTERPRISE_HOSTNAME']}/api/v3/",
-    repo: repo_name,
-    directory: directory,
-    branch: branch_name,
-  )
-elsif ENV["GITLAB_ACCESS_TOKEN"]
-  gitlab_hostname = ENV["GITLAB_HOSTNAME"] || "gitlab.com"
+#   source = Dependabot::Source.new(
+#     provider: "github",
+#     hostname: ENV["GITHUB_ENTERPRISE_HOSTNAME"],
+#     api_endpoint: "https://#{ENV['GITHUB_ENTERPRISE_HOSTNAME']}/api/v3/",
+#     repo: repo_name,
+#     directory: directory,
+#     branch: branch_name,
+#   )
+# elsif ENV["GITLAB_ACCESS_TOKEN"]
+#   gitlab_hostname = ENV["GITLAB_HOSTNAME"] || "gitlab.com"
 
-  credentials << {
-    "type" => "git_source",
-    "host" => gitlab_hostname,
-    "username" => "x-access-token",
-    "password" => ENV["GITLAB_ACCESS_TOKEN"] # A GitLab access token with API permission
-  }
+#   credentials << {
+#     "type" => "git_source",
+#     "host" => gitlab_hostname,
+#     "username" => "x-access-token",
+#     "password" => ENV["GITLAB_ACCESS_TOKEN"] # A GitLab access token with API permission
+#   }
 
-  source = Dependabot::Source.new(
-    provider: "gitlab",
-    hostname: gitlab_hostname,
-    api_endpoint: "https://#{gitlab_hostname}/api/v4",
-    repo: repo_name,
-    directory: directory,
-    branch: branch_name,
-  )
-elsif ENV["AZURE_ACCESS_TOKEN"]
-  azure_hostname = ENV["AZURE_HOSTNAME"] || "dev.azure.com"
+#   source = Dependabot::Source.new(
+#     provider: "gitlab",
+#     hostname: gitlab_hostname,
+#     api_endpoint: "https://#{gitlab_hostname}/api/v4",
+#     repo: repo_name,
+#     directory: directory,
+#     branch: branch_name,
+#   )
+# elsif ENV["AZURE_ACCESS_TOKEN"]
+#   azure_hostname = ENV["AZURE_HOSTNAME"] || "dev.azure.com"
 
-  credentials << {
-    "type" => "git_source",
-    "host" => azure_hostname,
-    "username" => "x-access-token",
-    "password" => ENV["AZURE_ACCESS_TOKEN"]
-  }
+#   credentials << {
+#     "type" => "git_source",
+#     "host" => azure_hostname,
+#     "username" => "x-access-token",
+#     "password" => ENV["AZURE_ACCESS_TOKEN"]
+#   }
 
-  source = Dependabot::Source.new(
-    provider: "azure",
-    hostname: azure_hostname,
-    api_endpoint: "https://#{azure_hostname}/",
-    repo: repo_name,
-    directory: directory,
-    branch: branch_name,
-  )
-else
-  source = Dependabot::Source.new(
-    provider: "github",
-    repo: repo_name,
-    directory: directory,
-    branch: branch_name,
-  )
-end
+#   source = Dependabot::Source.new(
+#     provider: "azure",
+#     hostname: azure_hostname,
+#     api_endpoint: "https://#{azure_hostname}/",
+#     repo: repo_name,
+#     directory: directory,
+#     branch: branch_name,
+#   )
+# else
+#   source = Dependabot::Source.new(
+#     provider: "github",
+#     repo: repo_name,
+#     directory: directory,
+#     branch: branch_name,
+#   )
+# end
+
+source = Dependabot::Source.new(
+  provider: "github",
+  repo: repo_name,
+  directory: directory,
+  branch: branch_name,
+)
 
 ##############################
 # Fetch the dependency files #
@@ -134,6 +141,26 @@ dependencies = parser.parse
 ####################################################
 puts "Retrieving vulnerabilities information"
 vulnerabilities = VulnerabilityFetcher.new(dependencies.map(&:name), package_manager, ENV["GITHUB_ACCESS_TOKEN"]).fetch_advisories
+
+####################################################
+# Create an Issue regard to the dependency metrics #
+####################################################
+lockfile = files.find{ |f| f.name == 'package-lock.json' }
+package_json = files.find{ |f| f.name == 'package.json' }
+if (not package_json.nil?) and (not lockfile.nil?)
+  print "Creating Ripple-Effect of Metrics dependency graph on Issue"
+  rem_issue_creator = Dependabot::RemIssueCreator.new(
+    source: source,
+    credentials: credentials,
+    custom_labels: ['dependency graph', 'javascript', 'rem', 'npm search metrics'],
+    lockfile: lockfile,
+    package_json: package_json,
+    metric: "final",
+    rem_api: "http://helium.cs.uvic.ca/rem/rem-with-lockfile-for-issue"
+  )
+  rem_issue_creator.create
+  puts " ..done"
+end
 
 dependencies.each do |dep|
   next if vulnerabilities[dep.name.to_sym].empty?
@@ -220,8 +247,8 @@ dependencies.each do |dep|
     assignees: [(ENV["PULL_REQUESTS_ASSIGNEE"] || ENV["GITLAB_ASSIGNEE_ID"])&.to_i],
     label_language: true,
     rem_graph_files: {
-      :package_json => files.find{ |f| f.name == 'package.json' }, 
-      :lockfile => files.find{ |f| f.name == 'npm-shrinkwrap.json' or f.name == 'package-lock.json' }
+      :package_json => package_json, 
+      :lockfile => lockfile
     },
     rem_graph_api: "http://helium.cs.uvic.ca/rem/rem-vulnerable-with-lockfile",
     vulnerabilities_fixed: vulnerabilities_fixed,
@@ -233,18 +260,18 @@ dependencies.each do |dep|
 
   # Enable GitLab "merge when pipeline succeeds" feature.
   # Merge requests created and successfully tested will be merge automatically.
-  if ENV["GITLAB_AUTO_MERGE"]
-    g = Gitlab.client(
-      endpoint: source.api_endpoint,
-      private_token: ENV["GITLAB_ACCESS_TOKEN"]
-    )
-    g.accept_merge_request(
-      source.repo,
-      pull_request.iid,
-      merge_when_pipeline_succeeds: true,
-      should_remove_source_branch: true
-    )
-  end
+  # if ENV["GITLAB_AUTO_MERGE"]
+  #   g = Gitlab.client(
+  #     endpoint: source.api_endpoint,
+  #     private_token: ENV["GITLAB_ACCESS_TOKEN"]
+  #   )
+  #   g.accept_merge_request(
+  #     source.repo,
+  #     pull_request.iid,
+  #     merge_when_pipeline_succeeds: true,
+  #     should_remove_source_branch: true
+  #   )
+  # end
 end
 
 puts "Done"
